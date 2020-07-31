e57012538ebc886a4074e8b0ffb1626060ae5e3d
Ian Kim <me@ian.kim>
2020-07-31 18:04:15 -0400

    Move splitWhitespaceNTimes() to common

--- src/blepBotMessageHandler.ts
+++ src/blepBotMessageHandler.ts
@@ -1,6 +1,6 @@
 import { Message, GuildChannel } from 'discord.js';
 import Command from 'command';
-import { error } from './common';
+import { error, splitWhitespaceNTimes } from './common';
 
 export default class BlepBotMessageHandler {
   client: any;
@@ -15,7 +15,7 @@ export default class BlepBotMessageHandler {
     if (!message.author.bot && message.content.startsWith(this.client.commandPrefix)) {
       const commandString = message.content.slice(this.client.commandPrefix.length);
       // eslint-disable-next-line prefer-const
-      let [command, argString] = this.splitWhitespaceNTimes(commandString, 1);
+      let [command, argString] = splitWhitespaceNTimes(commandString, 1);
       let handler: Command = this.client.commands.get(command);
       let parentHandler: Command;
       if (!handler) {
@@ -24,7 +24,7 @@ export default class BlepBotMessageHandler {
         error(message, 'This command can only be executed in a server.');
       } else {
         while (handler.subcommands?.length > 0) {
-          const [subcommand, subcommandArgString] = this.splitWhitespaceNTimes(argString, 1);
+          const [subcommand, subcommandArgString] = splitWhitespaceNTimes(argString, 1);
           const subcommandIndex = handler.subcommands.map((sc) => sc.name).indexOf(subcommand);
           if (subcommandIndex >= 0) {
             parentHandler = handler;
@@ -38,19 +38,17 @@ export default class BlepBotMessageHandler {
           if (argString?.length > 0) {
             error(message, `Unexpected arguments: \`${argString}\``);
           } else {
-            handler.execute(message, []);
+            handler.execute.apply(parentHandler || handler, [message, []]);
           }
         } else {
           let canExecute = true;
-          const args = this.splitWhitespaceNTimes(argString, handler.arguments.length - 1);
+          const args = splitWhitespaceNTimes(argString, handler.arguments.length - 1);
           handler.arguments.forEach((argument, i) => {
             if (!argument.optional && (!args[i] || args[i].length === 0)) {
               error(message, `Required argument \`${handler.arguments[i].name}\` was empty.`);
               canExecute = false;
             } else if (i === handler.arguments.length - 1 && !argument.infinite) {
-              const extraArgs = this
-                .splitWhitespaceNTimes(argString, handler.arguments.length)
-                .pop();
+              const extraArgs = splitWhitespaceNTimes(argString, handler.arguments.length).pop();
               if (extraArgs) {
                 error(message, `Unexpected extra arguments: \`${extraArgs}\``);
                 canExecute = false;
@@ -75,37 +73,6 @@ export default class BlepBotMessageHandler {
           this.client.triggers.get(regex).execute(message);
         }
       });
-    }
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  private splitWhitespaceNTimes(toSplit: string, times: number): string[] {
-    if (times < 0) {
-      throw new Error('Number of times cannot be negative');
-    } else if (!toSplit) {
-      return [];
-    } else if (times === 0) {
-      return [toSplit];
-    } else {
-      const splits: string[] = [];
-      let remnant = toSplit;
-      let timesRemaining = times;
-      let match;
-      do {
-        match = remnant.match(/\s+/);
-        if (match) {
-          splits.push(remnant.slice(0, match.index));
-          remnant = remnant.slice(match.index + match[0].length);
-          timesRemaining -= 1;
-        }
-      } while (match && timesRemaining > 0);
-      if (remnant) {
-        splits.push(remnant);
-      }
-      for (timesRemaining; timesRemaining > 0; timesRemaining -= 1) {
-        splits.push(null);
-      }
-      return splits;
     }
   }
 }
--- src/common.ts
+++ src/common.ts
@@ -7,3 +7,33 @@ export const error = (message: Message, errorMessage: string) => {
 export const success = (message: Message, successMessage: string) => {
   message.channel.send(`✅ ${successMessage}`);
 };
+
+export const splitWhitespaceNTimes = (toSplit: string, times: number): string[] => {
+  if (times < 0) {
+    throw new Error('Number of times cannot be negative');
+  } else if (!toSplit) {
+    return [];
+  } else if (times === 0) {
+    return [toSplit];
+  } else {
+    const splits: string[] = [];
+    let remnant = toSplit;
+    let timesRemaining = times;
+    let match;
+    do {
+      match = remnant.match(/\s+/);
+      if (match) {
+        splits.push(remnant.slice(0, match.index));
+        remnant = remnant.slice(match.index + match[0].length);
+        timesRemaining -= 1;
+      }
+    } while (match && timesRemaining > 0);
+    if (remnant) {
+      splits.push(remnant);
+    }
+    for (timesRemaining; timesRemaining > 0; timesRemaining -= 1) {
+      splits.push(null);
+    }
+    return splits;
+  }
+};
