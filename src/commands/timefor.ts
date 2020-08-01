fc1b86c075a372e87318380a85ea9bcd1fd159f3
Ian Kim <me@ian.kim>
2020-08-01 10:02:26 -0400

    minor: rename argument name to be more accurate

--- src/commands/timefor.ts
+++ src/commands/timefor.ts
@@ -29,7 +29,7 @@ export default class TimeForCommand extends BlepBotCommand {
       execute: this.setTimezone,
       arguments: [
         {
-          name: 'timezone',
+          name: 'cityName',
           infinite: true,
         },
       ],
